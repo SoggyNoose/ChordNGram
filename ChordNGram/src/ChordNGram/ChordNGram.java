1c327bf64093a4f89aa0c943f29fa8520fb1c6e1
Sam Kim <kimsj@rose-hulman.edu>
2012-02-03 13:16:54 -0500

    Formed Regex

--- ChordNGram/src/ChordNGram/ChordNGram.java
+++ ChordNGram/src/ChordNGram/ChordNGram.java
@@ -1,8 +1,41 @@
 package ChordNGram;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ChordNGram {
 
+
+	private static Pattern chord = Pattern.compile("[VIvi]+((M7)|(m7)|(sus))?");
+	
 	public static void main(String[] args) {
+		BufferedReader reader;
 		
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		String chordProg;
+		boolean running = true;
+		
+		while (running) {
+			System.out.print("Input Chord progression (or type 'quit'): ");
+			try {
+				chordProg = reader.readLine();
+				if (chordProg.equals("quit")) {
+					running = false;
+					break;
+				}
+				
+				Matcher matcher = chord.matcher(chordProg);
+				
+				while (matcher.find()) {
+					System.out.println("I found the chord: " + matcher.group());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
