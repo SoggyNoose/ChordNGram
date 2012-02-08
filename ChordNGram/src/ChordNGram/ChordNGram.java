4ba231be533db41739c4b3f6774196ca9ec52e6c
Sam Kim <kimsj@rose-hulman.edu>
2012-02-08 14:06:34 -0500

    Some things were changes

--- ChordNGram/src/ChordNGram/ChordNGram.java
+++ ChordNGram/src/ChordNGram/ChordNGram.java
@@ -1,26 +1,63 @@
 package ChordNGram;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ChordNGram {
+
+	private static String[] genres = { "Indie", "Folk", "Pop", "Alternative", "Metal", "Classic", "Punk", "Progressive" };
+	
+	public static void loadNGrams(String fname) {
+		// TODO
+		try {
+			BufferedReader file = new BufferedReader(new FileReader(fname));
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static void main(String[] args) {
 		BufferedReader reader;
 		
 		reader = new BufferedReader(new InputStreamReader(System.in));
 		
+		String option;
+		String fname;
+		String genre;
 		String chordProg;
 		boolean running = true;
 		
 		while (running) {
-			System.out.print("Input Chord progression (or type 'quit'): ");
+			System.out.println("Choose option: ");
+			System.out.println("1. Train Model");
+			System.out.println("2. Score Progression");
+			System.out.println("3. Quit");
+			System.out.print(">>");
 			try {
-				chordProg = reader.readLine();
-				if (chordProg.equals("quit")) {
+				option = reader.readLine();
+				
+				if (option.equals("1")) {
+					System.out.print("Please enter filename: ");
+					fname = reader.readLine();
+					// TODO Open the file and such
+					System.out.print("What genre is this song?");
+					genre = reader.readLine();
+					// Retrieve appropriate NGram model
+					// whatever.addProgression(something)
+				}
+				if (option.equals("2")) {
+					System.out.print("Please enter progression: ");
+					chordProg = reader.readLine();
+					// TODO score and such
+				}
+				if (option.equals("3")) {
+					System.out.println("Bye");
+					// TODO closing and such
 					running = false;
-					break;
 				}
 				
 			} catch (IOException e) {
--- ChordNGram/src/ChordNGram/NGram.java
+++ ChordNGram/src/ChordNGram/NGram.java
@@ -14,7 +14,6 @@ public class NGram implements Serializable {
 	public String genre;
 	public Map<BiGram, Integer> ngramTable = new HashMap<BiGram, Integer>();
 	public Map<String, Integer> contextTable = new HashMap<String, Integer>();
-	public int count;
 	
 	/**
 	 * 
@@ -25,11 +24,48 @@ public class NGram implements Serializable {
 		this.genre = genre;
 	}
 	
-	public void parseProgression(final String chordProg) {
-		List<String> chords = parseContexts(chordProg);
+	public void scoreProgression(final String chordProg) {
+		List<String> chords = parseChords(chordProg);
+		
+		// Create list of BiGrams
+		List<BiGram> sequence = new ArrayList<BiGram>();
+		for (int i=1; i<chords.size(); i++) {
+			BiGram bigram = new BiGram(chords.get(i-1), chords.get(i));
+			sequence.add(bigram);
+		}
+		
+		// Check for missing bigrams, updating if needed
+		checkMissingBigram(sequence);
+		
+		// Score
+		double score = 1;
+		for (BiGram bigram : sequence) {
+			score *= getBiGramLikelihood(bigram);
+		}
+	}
+	
+	public void addProgression(final String chordProg) {
+		List<String> chords = parseChords(chordProg);
+		
+		for (int i=1; i<chords.size(); i++) {
+			BiGram bigram = new BiGram(chords.get(i-1), chords.get(i));
+			Integer count = ngramTable.get(bigram);
+			if (count == null) {
+				ngramTable.put(bigram, 1);
+			} else {
+				ngramTable.put(bigram, count+1);
+			}
+			
+			Integer context = contextTable.get(bigram.chord1);
+			if (context == null) {
+				contextTable.put(bigram.chord1, 1);
+			} else {
+				contextTable.put(bigram.chord1, context+1);
+			}
+		}
 	}
 
-	public List<String> parseContexts(final String chordProg) {
+	public List<String> parseChords(final String chordProg) {
 		List<String> chords = new ArrayList<String>();
 		
 		Matcher matcher = chord.matcher(chordProg);
@@ -41,17 +77,52 @@ public class NGram implements Serializable {
 		return chords;
 	}
 	
+	public void checkMissingBigram(List<BiGram> sequence) {
+		List<BiGram> missingBgram = new ArrayList<BiGram>();
+		List<String> missingContext = new ArrayList<String>();
+		boolean hasMissing = false;
+		
+		for (BiGram bigram : sequence) {
+			Integer biGramCount = this.ngramTable.get(bigram);
+			Integer contextCount = this.contextTable.get(bigram.chord1);
+			
+			if(contextCount == null)
+			{
+				hasMissing = true;
+				missingContext.add(bigram.chord1);
+			}
+			if (biGramCount == null) {
+				hasMissing = true;
+				missingBgram.add(bigram);
+			}
+		}
+		
+		if (hasMissing) {
+			/* Laplace Add One  -- Should be replaced with backing off soon */
+			for (BiGram key : ngramTable.keySet()) {
+				ngramTable.put(key, ngramTable.get(key)+1);
+			}
+			for (String key : contextTable.keySet()) {
+				contextTable.put(key, contextTable.get(key)+1);
+			}
+			
+			// Add missing contexts to table
+			for (String context : missingContext) {
+				contextTable.put(context, 1);
+			}
+			
+			// Add missing bigrams to table
+			for (BiGram bigram : missingBgram) {
+				ngramTable.put(bigram, 1);
+			}
+		}
+	}
+	
 	public double getBiGramLikelihood(BiGram toGet)
 	{
 		Integer biGramCount = this.ngramTable.get(toGet);
-		int newCount = this.count;
-		/* Laplace Add One  -- Should be replaced with backing off soon */
-		if(biGramCount == 0)
-		{
-			biGramCount++;
-			newCount += ngramTable.size();
-		}
-		return (double)biGramCount/(double)newCount;
+		int contextCount = this.contextTable.get(toGet.chord1);
+		return (double)biGramCount/(double)contextCount;
 	}
 	
 	
